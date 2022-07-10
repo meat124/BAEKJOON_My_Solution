9100da2ead60a979441c90661dab4f79c389f7fa
Hyunjin Park <meat124@naver.com>
2022-07-10 19:41:43 +0900

    2022_07_10 백준 2579번(효율적으로 수정)

--- Silver_3/백준_2579_계단오르기.cpp
+++ Silver_3/백준_2579_계단오르기.cpp
@@ -1,26 +1,23 @@
 #include <iostream>
-#include <vector>
-#include <algorithm>
-
+using namespace std;
+int DP[302];    // DP[i] 는 i 번째 계단까지 얻을 수 있는 점수의 최대값
+int point[302]; // 각 계단의 점수
 int main()
 {
-    int stairs;
-    std::cin >> stairs;
-    std::vector<int> stair_points(stairs + 1);
-    std::vector<int> stair_points_current(stairs + 1);
-    for (size_t i = 0; i < stairs; i++)
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int N;
+    cin >> N;
+    for (size_t i = 1; i <= N; i++)
+        cin >> point[i];
+    DP[1] = point[1];
+    DP[2] = point[1] + point[2];
+    DP[3] = max(point[1] + point[3], point[2] + point[3]);
+    for (size_t i = 4; i <= N; i++)
     {
-        std::cin >> stair_points[i];
+        // DP[i] = DP[i - 2] + point[i]
+        // DP[i] = DP[i - 3] + point[i - 1] + point[i] 중 최대값
+        DP[i] = max(DP[i - 2] + point[i], DP[i - 3] + point[i - 1] + point[i]);
     }
-    stair_points_current[0] = stair_points[0];
-    stair_points_current[1] = stair_points[0] + stair_points[1];
-    // 첫번째 계단은 꼭 밟을 필요 없으므로 0 과 1 요소 중 max 를 선택한다.
-    stair_points_current[2] = std::max(stair_points[0], stair_points[1]) + stair_points[2];
-    for (size_t i = 3; i < stairs; i++)
-    {
-        // 둘 다 모두 stair_point[i] 를 더하므로 결국 마지막 계단을 무조건 밟게 되어있다.
-        stair_points_current[i] = std::max(stair_points_current[i - 2] + stair_points[i],
-                                           stair_points_current[i - 3] + stair_points[i - 1] + stair_points[i]);
-    }
-    std::cout << stair_points_current[stairs - 1];
+    cout << DP[N];
 }
