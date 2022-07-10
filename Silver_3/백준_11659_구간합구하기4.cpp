001036dcd13cd988cf568900e9d16ef6e071b312
Hyunjin Park <meat124@naver.com>
2022-07-10 21:34:52 +0900

    2022_07_10 백준 11659번(DP 를 이용하여 효율적으로 수정)

--- Silver_3/백준_11659_구간합구하기4.cpp
+++ Silver_3/백준_11659_구간합구하기4.cpp
@@ -1,46 +1,26 @@
 #include <iostream>
-#include <vector>
-std::vector<int> vec;
-int N, M;
-long long total_sum = 0;
-
-long long Sum(int start, int end)
-{
-    long long sum = 0;
-    int range = end - start + 1;
-    if (range <= N / 2)
-        for (auto itr = vec.begin() + start; itr != vec.begin() + end + 1; ++itr)
-            sum += *itr;
-    else
-    {
-        sum = total_sum;
-        for (auto itr = vec.begin(); itr != vec.begin() + start; ++itr)
-            sum -= *itr;
-        for (auto itr = vec.begin() + end + 1; itr != vec.end(); ++itr)
-            sum -= *itr;
-    }
-    return sum;
-}
+using namespace std;
+int arr[100002];
+int DP[100002]; // DP[i] 는 arr[1] ~ arr[i] 까지의 합
 
 int main()
 {
-    std::ios::sync_with_stdio(false);
-    std::cin.tie(NULL);
-    std::cin >> N >> M;
-
-    int n;
-    for (size_t i = 0; i < N; i++)
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int N, M;
+    cin >> N >> M;
+    for (size_t i = 1; i <= N; i++)
+        cin >> arr[i];
+    DP[0] = 0;
+    DP[1] = arr[1];
+    for (size_t i = 2; i <= N; i++)
     {
-        std::cin >> n;
-        vec.push_back(n);
-        total_sum += n;
+        DP[i] = DP[i - 1] + arr[i];
     }
-
-    std::pair<int, int> range;
-
-    for (size_t k = 0; k < M; k++)
+    pair<int, int> tmp;
+    for (size_t i = 0; i < M; i++)
     {
-        std::cin >> range.first >> range.second;
-        std::cout << Sum(range.first - 1, range.second - 1) << "\n";
+        cin >> tmp.first >> tmp.second;
+        cout << DP[tmp.second] - DP[tmp.first - 1] << "\n";
     }
 }
