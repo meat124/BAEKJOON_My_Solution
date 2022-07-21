1a30bb654f6939d7adda7257ad4935787e7faec4
Hyunjin Park <meat124@naver.com>
2022-07-21 14:19:49 +0900

    2022_07_21 백준 1759번

---
+++ Gold_5/백준_1759_암호만들기.cpp
@@ -0,0 +1,71 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+int L , C;
+vector<char> alpha;
+vector<char> password;
+char arr[5] = { 'a', 'e', 'i', 'o', 'u' };
+bool visit[16];
+
+void Print()
+{
+    for (auto itr : password)
+        cout << itr;
+    cout << "\n";
+}
+
+// password 에 자음이 2개, 모음이 1개 이상 존재하는지 확인하는 함수
+bool Is_right()
+{
+    int cnt = 0;
+    for (auto itr : password)
+        for (int i = 0;i < 5;i++)
+            if (itr == arr[i])
+                cnt++;
+    if (cnt > 0 && L - cnt > 1)
+        return true;
+    return false;
+}
+
+void DFS(int level , int cur)
+{
+    if (level == L)
+    {
+        if (Is_right())
+            Print();
+        return;
+    }
+    for (int i = cur + 1;i < C;i++)
+    {
+        if (visit[i])
+            continue;
+        visit[i] = true;
+        password.push_back(alpha[i]);
+        DFS(level + 1 , i);
+        visit[i] = false;
+        password.pop_back();
+    }
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cin >> L >> C;
+    for (int i = 0;i < C;i++)
+    {
+        char c;
+        cin >> c;
+        alpha.push_back(c);
+    }
+    sort(alpha.begin() , alpha.end());
+    for (int i = 0;i < C - L + 1;i++)
+    {
+        password.push_back(alpha[i]);
+        visit[i] = true;
+        DFS(1 , i);
+        password.pop_back();
+        visit[i] = false;
+    }
+}
