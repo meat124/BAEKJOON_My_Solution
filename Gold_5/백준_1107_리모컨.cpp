b1e14a32242a8c834f7692152522a63f2f9bafbf
Hyunjin Park <meat124@naver.com>
2022-07-29 20:46:52 +0900

    2022_07_29 백준 1107번

--- Gold_5/백준_1107_리모컨.cpp
+++ Gold_5/백준_1107_리모컨.cpp
@@ -1,76 +1,51 @@
 #include<iostream>
-#include<vector>
 #include<string>
+#include<vector>
 using namespace std;
-int N , M;
-string N_str; // 원하는 채널 string 타입
-int available_num[10]; // 고장난 번호는 1로 표시
-string make_num;
-int result = 50000000;
 
-void Make_min_num()
+int N , M;
+vector<int> D;
+bool disability_num[10];
+
+// 인자로 받은 숫자에 사용 불가능한 번호가 있다면 false, 없다면 true 를 리턴하는 함수
+bool Check(int cur)
 {
-    // 자릿수가 같아지면 result 와 값 비교하고 작은 값으로 초기화 한 후 종료
-    if (make_num.length() == N_str.length())
-    {
-        int min_cnt = make_num.length() + abs(stoi(make_num) - N);
-        result = result < min_cnt ? result : min_cnt;
-        return;
-    }
-    for (int i = 0;i <= 9;i++)
-    {
-        // 만약 고장난 버튼이면 continue
-        if (available_num[i] == 1)
-            continue;
-        make_num.push_back(i + 48);
-        Make_min_num();
-        make_num.pop_back();
-    }
+    string s_cur = to_string(cur); // 현재 탐색하고 있는 번호를 string 으로 캐스팅
+
+    for (int i = 0;i < s_cur.length();i++)
+        if (disability_num[s_cur[i] - '0']) // 번호에 고장난 번호가 들어가 있다면
+            return false; // false 를 리턴한다.
+    return true; // 고장난 번호가 없다면 true 를 리턴한다.
 }
 
 int main()
 {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-    cin >> N;
-    N_str = to_string(N);
-    cin >> M;
+    cin >> N >> M;
+
+    int tmp;
     for (int i = 0;i < M;i++)
     {
-        int n;
-        cin >> n;
-        available_num[n] = 1;
+        cin >> tmp;
+        disability_num[tmp] = true; // 사용 불가한 번호 true 로 초기화
     }
-    // 가까운 수를 구한 뒤, 그 수와 N 의 차를 구한다.
-    Make_min_num();
-    vector<int> available;
-    for (int i = 0;i < 10;i++)
+
+    string str_N = to_string(N);
+
+    int min_cnt = abs(N - 100); // 100에서 이동하는 횟수를 시작값으로 설정
+
+    // 0 부터 10^6까지 모든 수를 전부 경우를 계산하여 최소 이동 횟수를 탐색한다.
+    for (int i = 0;i <= 1000000;i++)
     {
-        if (available_num[i] == 0)
-            available.push_back(i);
-    }
-    // 자릿수가 다른 수에서 최소값이 나오는 경우 예외 처리
-    if (!available.empty())
-    {
-        int min_num = available.front();
-        int max_num = available.back();
-        string tmp_min , tmp_max;
-        for (int i = 0;i < N_str.length() + 1;i++)
-            tmp_min += (min_num + 48);
-        for (int i = 0;i < N_str.length() - 1;i++)
-            tmp_max += (max_num + 48);
-        // stoi() std::invalid_argument error
-        try {
-            int tmp = tmp_min.length() + abs(stoi(tmp_min) - N);
-            result = result < tmp ? result : tmp;
-            tmp = tmp_max.length() + abs(stoi(tmp_max) - N);
-            result = result < tmp ? result : tmp;
+        if (Check(i)) // 만약 고장난 번호가 없다면 최소값을 계산한다.
+        {
+            // N 까지의 1씩 이동 횟수 + 현재 i 를 입력하는데 필요한 횟수
+            tmp = abs(N - i) + to_string(i).length();
+            min_cnt = min(min_cnt , tmp);
         }
-        catch (std::exception) {
-            cout << result;
-            return 0;
-        }
+        // i 에 고장난 번호가 있다면 그 번호는 카운트 할 필요가 없다.
     }
-    result = result < abs(N - 100) ? result : abs(N - 100);
-    cout << result;
+
+    cout << min_cnt;
 }
