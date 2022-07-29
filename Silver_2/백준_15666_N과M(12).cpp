fc024b7ff9349b3f3d8ccf23bdc78ab7a0fbade1
Hyunjin Park <meat124@naver.com>
2022-07-30 08:09:29 +0900

    2022_07_30 백준 15666번

---
+++ Silver_2/백준_15666_N과M(12).cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+#include<algorithm>
+#include<vector>
+using namespace std;
+int N , M;
+vector<int> vec;
+vector<int> S;
+
+void DFS(int start)
+{
+    if (S.size() == M)
+    {
+        for (auto i : S)
+            cout << i << " ";
+        cout << "\n";
+        return;
+    }
+    int prev_num = -1;
+    for (int i = start;i < N;i++)
+    {
+        if (prev_num == vec[i])
+            continue;
+        S.push_back(vec[i]);
+        prev_num = vec[i];
+        DFS(i);
+        S.pop_back();
+    }
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cin >> N >> M;
+    for (int i = 0;i < N;i++)
+    {
+        int n;
+        cin >> n;
+        vec.push_back(n);
+    }
+    sort(vec.begin() , vec.end());
+
+    DFS(0);
+}
