7f8d6ad880102456b9ad354649be9155585b8961
Hyunjin Park <meat124@naver.com>
2022-08-05 11:51:35 +0900

    2022_08_05 백준 18870번 unique 함수를 이용한 개선 ver

--- Silver_2/백준_18870_좌표압축.cpp
+++ Silver_2/백준_18870_좌표압축.cpp
@@ -6,7 +6,6 @@ using namespace std;
 int N;
 vector<int> coord(1000005);
 vector<int> sorted_coord;
-set<int> remove_overlap;
 
 int main()
 {
@@ -16,10 +15,13 @@ int main()
     for (int i = 0;i < N;i++)
     {
         cin >> coord[i];
-        remove_overlap.insert(coord[i]);
+        sorted_coord.push_back(coord[i]);
     }
-    for (auto i : remove_overlap)
-        sorted_coord.push_back(i);
+    // unique 멤버 함수는 중복을 제거하고 뒤를 쓰레기 값으로 초기화 한다.
+    // 그리고 쓰레기 값이 시작하는 지점의 iterator 를 반환한다.
+    // unique 함수는 컨테이너가 정렬되어 있어야 정상적으로 작동한다.
+    sort(sorted_coord.begin() , sorted_coord.end());
+    sorted_coord.erase(unique(sorted_coord.begin() , sorted_coord.end()) , sorted_coord.end());
     for (int i = 0;i < N;i++)
         cout << lower_bound(sorted_coord.begin() , sorted_coord.end() , coord[i]) - sorted_coord.begin() << " ";
 }
