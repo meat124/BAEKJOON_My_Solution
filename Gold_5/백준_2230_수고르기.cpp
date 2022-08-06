4d4ded044315d35c90a124c46463eeb6f185a779
Hyunjin Park <meat124@naver.com>
2022-08-06 18:15:33 +0900

    2022_08_06 백준 2230번 two_pointer ver

--- Gold_5/백준_2230_수고르기.cpp
+++ Gold_5/백준_2230_수고르기.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 int N , M;
 int seq[100005];
-int result = 0x7FFFFFFF;
+int result = 0x7fffffff;
 
 int main()
 {
@@ -13,14 +13,16 @@ int main()
     for (int i = 0;i < N;i++)
         cin >> seq[i];
     sort(seq , seq + N);
-    // 이 값을 넘기면 더 이상 계산x
-    int max_num = seq[N - 1] - M;
-    for (int i = 0;i < N;i++)
+    int end = 0;
+    // start 가 0 ~ N - 1 까지 증가하면서 탐색한다.
+    for (int start = 0;start < N;start++)
     {
-        if (seq[i] > max_num)
+        // 만약 end 가 N 보다 작고, 차이가 M 보다 작다면
+        while (end < N && seq[end] - seq[start] < M)
+            end++;
+        if (end == N)
             break;
-        // seq[i] + M 보다 크거나 같은 값이 나타나는 첫 지점의 iterator 를 리턴한다.
-        result = min(result , seq[lower_bound(seq , seq + N , seq[i] + M) - seq] - seq[i]);
+        result = min(result , seq[end] - seq[start]);
     }
     cout << result;
 }
