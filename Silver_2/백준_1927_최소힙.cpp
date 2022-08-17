12930cdf64fa0ecb8d874564fb70548b371b5ead
Hyunjin Park <meat124@naver.com>
2022-08-17 17:48:02 +0900

    2022_08_17 백준 1927번 priority_queue STL 사용

--- Silver_2/백준_1927_최소힙.cpp
+++ Silver_2/백준_1927_최소힙.cpp
@@ -1,40 +1,28 @@
 #include<iostream>
-#include<map>
+#include<vector>
+#include<queue>
+#include<algorithm>
 using namespace std;
+priority_queue<int , vector<int> , greater<int>> pq; // 최소 힙
 int N;
-unsigned int min_value = (1 << 31);
-map<int , int> arr;
-// vector 를 사용하면 시간복잡도 초과(front 에서 팝 하는 경우)
 int main()
 {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     cin >> N;
-
-    for (int i = 0;i < N;i++)
+    int op;
+    while (N--)
     {
-        int x;
-        cin >> x;
-        if (x == 0)
-        {
-            if (arr.empty())
-                cout << 0 << "\n";
+        cin >> op;
+        if (op == 0)
+            if (pq.empty())
+                cout << "0\n";
             else
             {
-                min_value = arr.begin()->first;
-                cout << min_value << "\n";
-                if (arr[min_value] == 1)
-                    arr.erase(min_value);
-                else
-                    arr[min_value]--;
+                cout << pq.top() << "\n";
+                pq.pop();
             }
-        }
         else
-        {
-            if (arr.find(x) == arr.end())
-                arr.insert({ x,1 });
-            else
-                arr[x]++;
-        }
+            pq.push(op);
     }
 }
