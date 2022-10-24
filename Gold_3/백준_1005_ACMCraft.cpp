308895b2f4a03dcf7b4966b166b2e2cf92fe713c
Hyunjin Park <meat124@naver.com>
2022-10-24 18:16:07 +0900

    2022_10_24 백준 1005번 코드 주석 추가

--- Gold_3/백준_1005_ACMCraft.cpp
+++ Gold_3/백준_1005_ACMCraft.cpp
@@ -24,28 +24,29 @@ int main()
             int a , b;
             cin >> a >> b;
             adj[a].push_back(b); // 방향을 반대로 푸시
-            deg[b]++;
+            deg[b]++; // b 건물을 짓기 위해 필요한 indeg 1증가
         }
         cin >> W;
 
         for (int i = 1;i <= N;i++)
-            if (deg[i] == 0)
+            if (deg[i] == 0) // 먼저 indeg 가 0인 것들은 바로 건설 가능하므로 큐에 푸시한다.
                 Q.push(i);
         copy(build_time , build_time + N + 1 , DP);
-
+        // 큐가 비어있을 때 까지 반복한다.
         while (!Q.empty())
         {
             int cur = Q.front();
             Q.pop();
             for (int nxt : adj[cur])
             {
-                deg[nxt]--;
-                DP[nxt] = max(DP[nxt] , DP[cur] + build_time[nxt]);
-                if (deg[nxt] == 0)
+                deg[nxt]--; // cur을 건설하면 indeg 가 1감소하는 것
+                DP[nxt] = max(DP[nxt] , DP[cur] + build_time[nxt]); // nxt 를 짓기 위해 걸리는 시간 갱신
+                if (deg[nxt] == 0) // 만약 nxt 를 위한 indeg 가 모두 사리지면 큐에 푸시
                     Q.push(nxt);
             }
         }
-        cout << DP[W] << "\n";
+        cout << DP[W] << "\n"; // 최종 목표 건물인 W 번 건물의 DP 값을 출력한다.
+        // 다음 TC 를 위해서 간선 초기화
         for (int i = 1;i <= N;i++)
             adj[i].clear();
     }
